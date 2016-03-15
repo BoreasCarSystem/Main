5a998a7b5a76d04751cb3c7ebed313b3b21c8445
aliccce <aliceg@outlook.com>
2016-03-15 11:07:37 +0100

    Added handle, run, poll.

--- CarCommunicator/Main.py
+++ CarCommunicator/Main.py
@@ -2,11 +2,13 @@ import requests
 from Temperature import Temperature
 from json import JSONDecodeError
 from Car import CarControl
+from time import sleep
 
 
 class Main:
 
     def __init__(self, car_api, port):
+        self.POLLING_INTERVAL = 20
         self.car_api = car_api
         self.port = port
         self.AC_controller = None
@@ -14,42 +16,38 @@ class Main:
         self.target_temp = 20
 
     def run(self):
-        r = self.send_data(self.get_error_message(), True)
+        while True:
+            self.poll()
+            sleep(self.POLLING_INTERVAL)
 
+    def poll(self):
+        # TODO: Send fresh car information to the server
+        self.handle(self.send_data(self.get_error_message(), True))
+
+    def handle(self, r):
         messages = {message['type']: message['value'] for message in r}
 
-        try:
-            if 'AC_temperature' in messages and self.AC_controller is not None:
+        if 'AC_temperature' in messages:
+            self.target_temp = messages['AC_temperature']
+            if self.AC_controller is not None:
                 # If AC-session is started, and user wants to update temperature
-                self.target_temp = messages['AC_temperature']
                 self.AC_controller.update_temperature(self.target_temp)
 
+        if 'AC_enabled' in messages:
             if messages['AC_enabled']:
-                # Update if new target_temp is set
-                if 'AC_temperature' in messages:
-                    self.target_temp = messages['AC_temperature']
-
                 # Activate AC by creating temperature object
                 self.AC_controller = Temperature(self.car_control, self.target_temp)
-
             else:
                 if self.car_control is not None:
                     # Deactivate AC by calling self.AC_controller.deactivate()
                     self.AC_controller.deactivate()
                     self.AC_controller = None
 
-        except KeyError:
-            print("SOMETHING WEN")
-
-
-
-
-
-
-
     def send_data(self, data, error=False):
         """
         Polls the CarAPI server for messages.
+
+        :param error: Set to true to make this an error message for the server.
         :param data: Data to be encoded as JSON and sent to the server.
         :return: JSON returned from server
         """
@@ -72,7 +70,6 @@ class Main:
                 # Nope, malformed data from server..?
                 raise e
 
-
     def get_error_message(self):
         """
         Return JSON-compatible object, placeholder since we don't
@@ -80,9 +77,6 @@ class Main:
         :return:
         """
         return {"errno": 3, "message": "Not implemented yet"}
-
-    def create_AC_session(self, target_temp):
-        pass
 
 
 if __name__ == '__main__':
